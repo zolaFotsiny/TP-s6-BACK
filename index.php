7ce25ab57553cb52694a9e3cef9f6ba7d5708782
zolaFotsiny <toavinaranarilala@gmail.com>
2022-05-16 23:16:14 +0300

    'finish'

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
         <form method="post" action='traitementLogin.php'>
             <h2 class="sr-only">Login Form</h2>
             <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
-            <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email"></div>
-            <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password">
+            <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email" values='admin@gmail.com'></div>
+            <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password" values='mdpprom13'>
             </div>
             <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Log In</button></div><a
                 href="#" class="forgot">Forgot your email or password?</a>
