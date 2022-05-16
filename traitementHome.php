42635933afc431dade42e4cd06bfce38fadf82e2
zolaFotsiny <toavinaranarilala@gmail.com>
2022-05-16 17:54:35 +0300

    make it better

--- traitementHome.php
+++ traitementHome.php
@@ -1,5 +1,13 @@
 <?php
-    include('Function.php');
+    
+   require'dbConnect.php';
+    
+    function insert($data){
+        $con = dbconnect();
+        $sql = "insert into Actualite (titre, sousTitre, description, date) values('%s', '%s', '%s', '%s')";
+        $sqlFinal = sprintf($sql,$data['titre'],$data['sousTitre'],$data['description'],$data['date']);   
+        $con->query($sqlFinal);
+    }
 
     $data['titre'] = $_POST['titre'];
     $data['sousTitre'] = $_POST['sousTitre'];
